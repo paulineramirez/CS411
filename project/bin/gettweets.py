bf63346cd0cf478dbab5465e3f8479cfccb35c42
hlwrocer <duanhanson@gmail.com>
2015-10-29 12:13:02 -0700

    moved get tweets into separate file

---
+++ project/bin/gettweets.py
@@ -0,0 +1,28 @@
+from TwitterSearch import *
+import json
+def getTweets(keyword1):
+  
+    search_order = TwitterSearchOrder()
+    search_order.set_keywords([keyword1])
+    search_order.set_count(2)
+    search_order.set_include_entities(False)
+
+    ts = TwitterSearch(
+            consumer_key = "BkczwDXiYQWAf2klUbnv2hEO0",
+            consumer_secret = "JZibmuDUVcQ6utlG9kcoujtbJKHByoC2uM26muO9dpG1K49Hnm",
+            access_token = "1051442228-Hrwir9aT8K8kFFg8zfiV9VfCW2QAEk47W5xZrRm",
+            access_token_secret = "9dLGcZTYkLnazEBbghnhIwNIXM1fjZDCzQkn2kn4NUUTY"
+  )
+    x = []
+    counter = 0
+    for happy_tweet in ts.search_tweets_iterable(search_order):
+        if counter < 10:
+             x.append('@%s tweeted: %s' % ( happy_tweet['user']['screen_name'], happy_tweet['text']))
+             counter += 1
+        else:
+            break
+    return x
+
+
+
+
