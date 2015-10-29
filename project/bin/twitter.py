e0bbecd6c66b389a5dd58275914494bc67f17d96
hlwrocer <duanhanson@gmail.com>
2015-10-29 12:10:41 -0700

    one tweet shows up inhtml

--- project/bin/twitter.py
+++ project/bin/twitter.py
@@ -1,12 +1,14 @@
-import json
 import web
-from TwitterSearch import *
+import gettweets
+
 urls = (
-    '/twitter', 'Tweets'
+    '/', 'Tweets'
+    ,'/search', 'Search'
     )
 app = web.application(urls, globals())
 
 render = web.template.render('templates/')
+searchQuery = "hasnotchanged"
 
 class Tweets(object):
     def GET(self):
@@ -14,31 +16,25 @@ class Tweets(object):
 
 
     def POST(self):
-        form = web.input(tweets="")
-        keyword1 = form.tweets 
-        #ts = getTweets(keyword1)
-        ts = "nvm"
+        form = web.input(tweets="testingthisthing")
+        global searchQuery
+        searchQuery = str(form.tweets)
+        raise web.redirect('/search') 
+     
+class Search:
+    def GET(self):
+        global searchQuery    
+
+        ts = gettweets.getTweets(searchQuery)
+        ts = ts[0]
         return render.index(ts = ts)
+       
+
+
 if __name__ == "__main__":
     app.run()
 
 
-def getTweets(keyword1):
-    search_order = TwitterSearchOrder()
-    search_order.set_keywords([keyword1])
-    search_order.set_count(2)
-    search_order.set_include_entities(False)
-
-    ts = TwitterSearch(
-            consumer_key = "BkczwDXiYQWAf2klUbnv2hEO0",
-            consumer_secret = "JZibmuDUVcQ6utlG9kcoujtbJKHByoC2uM26muO9dpG1K49Hnm",
-            access_token = "1051442228-Hrwir9aT8K8kFFg8zfiV9VfCW2QAEk47W5xZrRm",
-            access_token_secret = "9dLGcZTYkLnazEBbghnhIwNIXM1fjZDCzQkn2kn4NUUTY"
-            )
-
-    for happy_tweet in ts.search_tweets_iterable(search_order):
-            print('@%s tweeted: %s' % ( happy_tweet['user']['screen_name'], happy_tweet['text']))
-    return ts
 '''
 tweetarray = []
 counter = 0
