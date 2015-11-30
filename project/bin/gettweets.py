ad595d62ffa8d9bf7acf4d7e49c91951335582df
hlwrocer <duanhanson@gmail.com>
2015-11-30 15:36:09 -0800

    fixed stuff

--- project/bin/gettweets.py
+++ project/bin/gettweets.py
@@ -13,12 +13,12 @@ def getTweets(keyword1):
             access_token = "1051442228-Hrwir9aT8K8kFFg8zfiV9VfCW2QAEk47W5xZrRm",
             access_token_secret = "9dLGcZTYkLnazEBbghnhIwNIXM1fjZDCzQkn2kn4NUUTY"
   )
-    x = "" 
+    x = [] 
     counter = 0
     for happy_tweet in ts.search_tweets_iterable(search_order):
         if counter < 10:
-             x += ('@%s tweeted: %s' % ( happy_tweet['user']['screen_name'], happy_tweet['text']))
-             x += " \n"
+             x += [('@%s tweeted: %s' % ( happy_tweet['user']['screen_name'], happy_tweet['text']))]
+             
              counter += 1
         else:
             break
