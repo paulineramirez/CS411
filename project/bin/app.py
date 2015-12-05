da8206ae06587fed7c77176158e0a00225698f88
dwangus <dwangus@bu.edu>
2015-12-05 02:58:44 -0500

    Added filter by tag functionality
    Wow that took a long time

--- project/bin/app.py
+++ project/bin/app.py
@@ -21,6 +21,7 @@ app = web.application(urls, globals())
 
 render = web.template.render('templates/', base = 'layout')
 searchQuery = "hasnotchangedlolol"
+tagList = []
 
 # Apply for funding form
 vemail = form.regexp(r".*@.*", "Must be a valid email address")
@@ -83,13 +84,17 @@ class Bar(object):
                     if len(form.bar) == 0:
 			return "Error: Enter a search with something!"
                     global searchQuery
-		    searchQuery = str(form.bar)
-		    urlquery = '/startups?query='+searchQuery
-		    raise web.redirect(urlquery)
+		    searchQuery = str(form.bar).strip()
+		    sQList = searchQuery.split(" ")
+                    urlquery = '/startups?query='
+                    for word in sQList:
+                        urlquery = urlquery + word + "_"
+                    urlquery = urlquery[:-1]
+                    raise web.redirect(urlquery)
                 if form.keys()[0] == "tweets":
                     if len(form.tweets) == 0:
                         return "Error: Enter a query to search for"
-                    searchQuery = str(form.tweets)
+                    searchQuery = str(form.tweets).strip()
                     raise web.redirect('/search')
 
 class Search:
@@ -110,34 +115,129 @@ class About:
 
 class Startups:
 	def GET(self):
-		global searchQuery
-		urlstuffs = web.input(filter="default")
-		sortFilter = urlstuffs.filter
+                global searchQuery
+		global tagList
+		urlstuffs = web.input(sort="default", query="default", tags="default")
+		sortFilter = urlstuffs.sort
+		if urlstuffs.query != "default" or urlstuffs.tags != "default":
+                        del tagList[:]
+                        q = urlstuffs.query
+                        t = urlstuffs.tags
+                        ql = q.split("_")
+                        tl = t.split("_")
+                        ql2 = []
+                        tl2 = []
+                        for word in ql:
+                                if word != unicode("", "utf-8"):
+                                        ql2.append(word)
+                        for tag in tl:
+                                if tag != unicode("", "utf-8"):
+                                        tl2.append(tag)
+                        if len(ql2) == 0:
+                                searchQuery = ""
+                        else:
+                                searchQuery = ""
+                                for index in range(len(ql2)):
+                                        ql2[index] = str(ql2[index])
+                                for word in ql2:
+                                        searchQuery = searchQuery + word + " "
+                                searchQuery = searchQuery[:-1]
+                        if len(tl2) != 0:
+                                for tag in tl2:
+                                        tagList.append(tag)
 		if searchQuery == "hasnotchangedlolol":
 			startupsTable = config.DB.select('startups').list()
+			if len(tagList) != 0:
+                                holder = []
+                                for item in startupsTable:
+                                        itemTags = (item.startup_category).split(",")
+                                        itemTags[0] = itemTags[0][6:]
+                                        itemTags.pop()
+                                        for index in range(1, len(itemTags)):
+                                                itemTags[index] = itemTags[index][1:]
+                                        for tag in tagList:
+                                             if (tag in itemTags) and (item not in holder):
+                                                     holder.append(item)
+                                del startupsTable[:]
+                                for item in holder:
+                                        startupsTable.append(item)
 			if sortFilter == "alphabetical":
 				startupsTable = sorted(startupsTable, key=lambda startup_item: startup_item.startup_name)
 			if len(startupsTable) > 9:
 				totalnumpages = math.ceil((float(len(startupsTable)) / float(9)))
-				return render.startups(startupsTable = startupsTable, numPages = int(totalnumpages), query = searchQuery, sort=sortFilter)
+				return render.startups(startupsTable = startupsTable, numPages = int(totalnumpages), query = searchQuery, sort=sortFilter, tagL = tagList, url = urlstuffs)
 			else:
-				return render.startups(startupsTable = startupsTable, numPages = 0, query = searchQuery, sort=sortFilter)
+				return render.startups(startupsTable = startupsTable, numPages = 0, query = searchQuery, sort=sortFilter, tagL = tagList, url = urlstuffs)
 		else:
 			StartupsTable = config.DB.select('startups').list()
 			newStartupsTable = []
 			passin = searchQuery
-			for item in StartupsTable:
-				if (searchQuery in str(item.startup_name).lower()) or (searchQuery in str(item.contact_name).lower()) or (searchQuery in str(item.startup_description).lower()):
-					newStartupsTable.append(item)
+			passinTags = []
+			sQList = searchQuery.split(" ")
+			for tag in tagList:
+                                passinTags.append(tag)
+                        del tagList[:]
+			if searchQuery == "":
+                                for item in StartupsTable:
+                                        newStartupsTable.append(item)
+                                passin = "hasnotchangedlolol"
+                        else:
+                                for item in StartupsTable:
+                                        for word in sQList:
+                                                wordlower = word.lower()
+                                                if (item not in newStartupsTable) and ((wordlower in str(item.startup_name).lower()) or (wordlower in str(item.contact_name).lower()) or (wordlower in str(item.startup_description).lower())):
+                                                        newStartupsTable.append(item)
+                        if len(passinTags) != 0:
+                                holder = []
+                                for item in newStartupsTable:
+                                        itemTags = (item.startup_category).split(",")
+                                        itemTags[0] = itemTags[0][6:]
+                                        itemTags.pop()
+                                        for index in range(1, len(itemTags)):
+                                                itemTags[index] = itemTags[index][1:]
+                                        for tag in passinTags:
+                                             if (tag in itemTags) and (item not in holder):
+                                                     holder.append(item)
+                                del newStartupsTable[:]
+                                for item in holder:
+                                        newStartupsTable.append(item)
 			if sortFilter == "alphabetical":
 				newStartupsTable = sorted(newStartupsTable, key=lambda startup_item: startup_item.startup_name)
 			if len(newStartupsTable) > 9:
 				searchQuery = "hasnotchangedlolol"
 				totalnumpages = math.ceil((float(len(newStartupsTable)) / float(9)))
-				return render.startups(startupsTable = newStartupsTable, numPages = int(totalnumpages), query = passin, sort=sortFilter)
+				return render.startups(startupsTable = newStartupsTable, numPages = int(totalnumpages), query = passin, sort=sortFilter, tagL = passinTags, url = urlstuffs)
 			else:
 				searchQuery = "hasnotchangedlolol"
-				return render.startups(startupsTable = newStartupsTable, numPages = 0, query = passin, sort=sortFilter)
+				return render.startups(startupsTable = newStartupsTable, numPages = 0, query = passin, sort=sortFilter, tagL = passinTags, url = urlstuffs)
+	def POST(self):
+                global searchQuery
+                form = web.input(SAF = [])
+                filters = form.SAF
+                searchQuery = str(filters[0]).strip()
+                filters.pop(0)
+                global tagList
+                
+                if searchQuery == "" and len(filters) == 0:
+                        return "Need to search for something with a filter!"
+                elif searchQuery == "" and len(filters) != 0:
+                        urlquery = '/startups?query=_&tags='
+                        for tag in filters:
+                                tagList.append(tag)
+                                urlquery = urlquery + str(tag) + '_'
+                        urlquery = urlquery[:-1]
+                else:
+                        sQList = searchQuery.split(" ")
+                        urlquery = '/startups?query='
+                        for word in sQList:
+                            urlquery += (word + "_")
+                        urlquery = urlquery[:-1]
+                        urlquery = urlquery + "&tags="
+                        for tag in filters:
+                                tagList.append(tag)
+                                urlquery = urlquery + str(tag) + "_"
+                        urlquery = urlquery[:-1]
+                raise web.redirect(urlquery)
 
 class RequestFunding:
     def GET(self):
